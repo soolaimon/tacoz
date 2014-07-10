53fc0d4fda7f180f5da9c496ce3c54695157c38b
Dave Nelson <davidicus21@gmail.com>
2014-07-10 11:11:35 -0600

    added menu items controller, added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get '/about' => 'site#about'
   get '/contact' => 'site#contact'
   post '/contact-submit' => 'site#contact_submit', as: :submit_contact
+  resources :menu_items, only: [:index, :show], path: 'our-food'
 end
+
