9a021716f5c4f248244e96d5ca81cc4227c99a2d
Dave Nelson <davidicus21@gmail.com>
2014-07-10 10:47:35 -0600

    contact mailer

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -10,4 +10,9 @@ class SiteController < ApplicationController
   def contact
     
   end
+
+  def contact_submit
+    ContactMailer.contact_request(params).deliver
+    redirect_to root_path, notice: "Thanks for contacting us!"
+  end
 end
