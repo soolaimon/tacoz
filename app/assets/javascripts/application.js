0ca997f056f312cc07e3341902bd9bcee068b5ce
Dave Nelson <davidicus21@gmail.com>
2014-07-11 11:50:52 -0600

    added map into location page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,30 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require gmaps
 //= require_tree .
+
+$(document).ready(function() {
+  loadMap();
+});
+
+var loadMap = function () {
+  $map = $('#map');
+  if($map.length) {
+    var gmap = new GMaps({
+      div: '#map',
+      lat: $map.data('lat'),
+      lng: $map.data('lon')
+    });
+
+    gmap.addMarker({
+      lat: $map.data('lat'),
+      lng: $map.data('lon'),
+      title: "Tacoz",
+      click: function(e) {
+        alert('This is the place');
+      }
+    });
+
+  }
+};
